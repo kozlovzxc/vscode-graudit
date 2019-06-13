12e35015b29e0ac14db928b3ac45a6ce13e8071c
Kozlov Nikita <kozlovzxc@gmail.com>
2019-06-13 22:56:03 +0300

    feat(extension): added file read on every event

--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,42 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+function check(textDocument: vscode.TextDocument) {
+	console.log(textDocument.getText());
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "graudit" is now active!');
+	console.log('Congratulations, your extension "graudit" is now active!');
+
+	function didOpenTextDocument(textDocument: vscode.TextDocument) {
+		console.log('didOpenTextDocument');
+		check(textDocument);
+	}
+
+	function didSaveTextDocument(textDocument: vscode.TextDocument) {
+		console.log('didSaveTextDocument');
+		check(textDocument);
+	}
+
+	const openedDisposable = vscode.workspace.onDidOpenTextDocument(didOpenTextDocument);
+	context.subscriptions.push(openedDisposable);
+	const savedDisposable = vscode.workspace.onDidSaveTextDocument(didSaveTextDocument);
+	context.subscriptions.push(savedDisposable);
+	
+	vscode.workspace.textDocuments.map(check);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	// let disposable = vscode.commands.registerCommand('graudit.run', () => {
+	// 	// The code you place here will be executed every time your command is executed
+	// });
+	// context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
